e6b04415ddb29e9700adb7d01f075da5d5b6e1c2
Yurrili <yurrili931@gmail.com>
2016-01-15 17:48:22 +0100

    no message

--- app/src/main/java/com/uj/yuri/budgetflow/NewIncomeOutcome_OutcomeFragment.java
+++ app/src/main/java/com/uj/yuri/budgetflow/NewIncomeOutcome_OutcomeFragment.java
@@ -1,17 +1,24 @@
 package com.uj.yuri.budgetflow;
 
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TimePicker;
 
 import com.codetroopers.betterpickers.calendardatepicker.CalendarDatePickerDialogFragment;
+import com.codetroopers.betterpickers.calendardatepicker.CalendarDatePickerDialogFragment;
 import com.codetroopers.betterpickers.datepicker.DatePickerBuilder;
+import com.codetroopers.betterpickers.datepicker.DatePickerDialogFragment;
 import com.codetroopers.betterpickers.timepicker.TimePickerBuilder;
 import com.uj.yuri.budgetflow.db_managment.DateBaseHelper_;
 import com.uj.yuri.budgetflow.db_managment.db_helper_objects.Category;
@@ -26,6 +33,83 @@ public class NewIncomeOutcome_OutcomeFragment extends Fragment {
     private Spinner spinner;
     public DateBaseHelper_ helper;
     private HashMap<String, Category> hashCat;
+    public static EditText date_place;
+    public static EditText time_place;
+    static final int DATE_DIALOG_ID = 999;
+    private static final String FRAG_TAG_DATE_PICKER = "fragment_date_picker_name";
+
+    public static class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            String d;
+            String m;
+
+            if( day < 10){
+                d = "0"+day;
+
+            }else {
+                d = day +"";
+            }
+            month = month +1;
+            if( month < 10){
+                m = "0"+ month;
+
+            }else {
+                m = month +"";
+            }
+             date_place.setText(d + "-" + m +  "-" + year);
+        }
+    }
+
+    public static class TimePickerFragment extends DialogFragment
+            implements TimePickerDialog.OnTimeSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int minutes = c.get(Calendar.MINUTE);
+
+
+            // Create a new instance of DatePickerDialog and return it
+            return new TimePickerDialog(getActivity(), this, hour, minutes, true);
+        }
+
+        public void onTimeSet(TimePicker view, int hours, int minutes) {
+            String h;
+            String m;
+
+            if( hours < 10){
+                h = "0"+hours;
+
+            }else {
+                h = hours +"";
+            }
+            if( minutes < 10){
+                m = "0"+ minutes;
+
+            }else {
+                m = minutes +"";
+            }
+
+            time_place.setText(h + ":" + m);
+        }
+    }
+
 
     public NewIncomeOutcome_OutcomeFragment() {
 
@@ -37,27 +121,22 @@ public class NewIncomeOutcome_OutcomeFragment extends Fragment {
         myFragmentView = inflater.inflate(R.layout.fragment_new_outcome, container, false);
         this.helper = new DateBaseHelper(getActivity());
         hashCat = helper.selectAllCategories();
-        EditText date_place =  (EditText) myFragmentView.findViewById(R.id.date_place);
+        date_place =  (EditText) myFragmentView.findViewById(R.id.date_place);
+        time_place =  (EditText) myFragmentView.findViewById(R.id.time_place);
+
+
 
         date_place.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                DatePickerBuilder dpb = new DatePickerBuilder()
-                        .setFragmentManager(getChildFragmentManager())
-                        .setStyleResId(R.style.BetterPickersDialogFragment)
-                        .setTargetFragment(NewIncomeOutcome_OutcomeFragment.this);
-                dpb.show();
+                showDatePickerDialog(v);
             }
         });
 
-        EditText time_place =  (EditText) myFragmentView.findViewById(R.id.time_place);
         time_place.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                TimePickerBuilder tpb = new TimePickerBuilder()
-                        .setFragmentManager(getChildFragmentManager())
-                        .setStyleResId(R.style.BetterPickersDialogFragment);
-                tpb.show();
+                showTimePickerDialog(v);
             }
         });
 
@@ -66,8 +145,6 @@ public class NewIncomeOutcome_OutcomeFragment extends Fragment {
         return myFragmentView;
     }
 
-
-
     void setSpinnerAdapter() {
         ArrayList<Category> cat = new ArrayList<>(hashCat.values());
         MySpinner adapter = new MySpinner(getActivity(),
@@ -75,6 +152,14 @@ public class NewIncomeOutcome_OutcomeFragment extends Fragment {
         spinner.setAdapter(adapter);
     }
 
+        public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getFragmentManager(), "datePicker");
+    }
 
+    public void showTimePickerDialog(View v) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getFragmentManager(), "timePicker");
+    }
 
 }
